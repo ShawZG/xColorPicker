b0af3a55db4e5b4db91f4d7891deca94ac5f57a9
xiaozhiguo <blueshaw@163.com>
2020-06-13 21:49:20 +0800

    调整目录结构

---
+++ src/english.ts
@@ -0,0 +1,206 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>ColorIndicator</name>
+    <message>
+        <location filename="ColorIndicator.cpp" line="35"/>
+        <source>Hex</source>
+        <translatorcomment>Hex</translatorcomment>
+        <translation>Hex</translation>
+    </message>
+</context>
+<context>
+    <name>MainDialog</name>
+    <message>
+<<<<<<< HEAD
+        <source>XColorPicker</source>
+        <translatorcomment>XColorPicker</translatorcomment>
+        <translation type="vanished">XColorPicker</translation>
+    </message>
+    <message>
+        <source>Author: blueshaw@163.com</source>
+        <translatorcomment>Author: blueshaw@163.com</translatorcomment>
+        <translation type="vanished">Author: blueshaw@163.com</translation>
+    </message>
+    <message>
+        <source>Pick</source>
+        <translatorcomment>Pick</translatorcomment>
+        <translation type="vanished">Pick</translation>
+    </message>
+    <message>
+        <source>Picker color</source>
+        <translatorcomment>Pick color</translatorcomment>
+        <translation type="vanished">Pick color</translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translatorcomment>Copy</translatorcomment>
+        <translation type="vanished">Copy</translation>
+    </message>
+    <message>
+        <source>Copy to clip</source>
+        <translatorcomment>Copy to clip</translatorcomment>
+        <translation type="vanished">Copy to clip</translation>
+    </message>
+    <message>
+        <location filename="MainDialog.cpp" line="60"/>
+        <source>XColorPicker </source>
+        <translatorcomment>XColorPicker </translatorcomment>
+        <translation>XColorPicker </translation>
+    </message>
+</context>
+<context>
+    <name>StackConfigFrame</name>
+    <message>
+        <location filename="StackConfigFrame.cpp" line="53"/>
+        <source>Language</source>
+        <translatorcomment>Language</translatorcomment>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <location filename="StackConfigFrame.cpp" line="58"/>
+        <source>English</source>
+        <translatorcomment>English</translatorcomment>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="StackConfigFrame.cpp" line="59"/>
+        <source>Simplified Chinese</source>
+        <translatorcomment>简体中文</translatorcomment>
+        <translation>简体中文</translation>
+    </message>
+    <message>
+        <location filename="StackConfigFrame.cpp" line="71"/>
+        <source>Skin</source>
+        <translatorcomment>Skin</translatorcomment>
+        <translation>Skin</translation>
+    </message>
+    <message>
+        <location filename="StackConfigFrame.cpp" line="76"/>
+        <source>Default</source>
+        <translatorcomment>Default</translatorcomment>
+        <translation>Default</translation>
+    </message>
+    <message>
+        <location filename="StackConfigFrame.cpp" line="88"/>
+        <source>Close Button</source>
+        <translatorcomment>Close Button</translatorcomment>
+        <translation>Close Button</translation>
+    </message>
+    <message>
+        <location filename="StackConfigFrame.cpp" line="92"/>
+        <source>Exit program</source>
+        <translatorcomment>Exit program</translatorcomment>
+        <translation>Exit program</translation>
+    </message>
+    <message>
+        <location filename="StackConfigFrame.cpp" line="93"/>
+        <source>Hide to system tray</source>
+        <translatorcomment>Hide to system tray</translatorcomment>
+        <translation>Hide to system tray</translation>
+    </message>
+    <message>
+        <location filename="StackConfigFrame.cpp" line="106"/>
+        <source>Return</source>
+        <translatorcomment>Return</translatorcomment>
+        <translation>Return</translation>
+    </message>
+    <message>
+        <location filename="StackConfigFrame.cpp" line="107"/>
+        <source>Return to picker dialog</source>
+        <translatorcomment>Return to picker dialog</translatorcomment>
+        <translation>Return to picker dialog</translation>
+    </message>
+    <message>
+        <source>Picker color</source>
+        <translatorcomment>Return to picker dialog</translatorcomment>
+        <translation type="vanished">Return to picker dialog</translation>
+    </message>
+</context>
+<context>
+    <name>StackPickerFrame</name>
+    <message>
+        <location filename="StackPickerFrame.cpp" line="106"/>
+        <source>Setting</source>
+        <translatorcomment>Setting</translatorcomment>
+        <translation>Setting</translation>
+    </message>
+    <message>
+        <location filename="StackPickerFrame.cpp" line="112"/>
+=======
+        <location filename="MainDialog.cpp" line="67"/>
+        <source>XColorPicker</source>
+        <translatorcomment>XColorPicker</translatorcomment>
+        <translation>XColorPicker</translation>
+    </message>
+    <message>
+        <location filename="MainDialog.cpp" line="115"/>
+>>>>>>> 4da7d9fe7b6f3bd9228dbcf5380533f8a39c9e1f
+        <source>Author: blueshaw@163.com</source>
+        <translatorcomment>Author: blueshaw@163.com</translatorcomment>
+        <translation>Author: blueshaw@163.com</translation>
+    </message>
+    <message>
+<<<<<<< HEAD
+        <location filename="StackPickerFrame.cpp" line="127"/>
+=======
+        <location filename="MainDialog.cpp" line="128"/>
+>>>>>>> 4da7d9fe7b6f3bd9228dbcf5380533f8a39c9e1f
+        <source>Pick</source>
+        <translatorcomment>Pick</translatorcomment>
+        <translation>Pick</translation>
+    </message>
+    <message>
+<<<<<<< HEAD
+        <location filename="StackPickerFrame.cpp" line="128"/>
+=======
+        <location filename="MainDialog.cpp" line="129"/>
+>>>>>>> 4da7d9fe7b6f3bd9228dbcf5380533f8a39c9e1f
+        <source>Picker color</source>
+        <translatorcomment>Pick color</translatorcomment>
+        <translation>Pick color</translation>
+    </message>
+    <message>
+<<<<<<< HEAD
+        <location filename="StackPickerFrame.cpp" line="132"/>
+=======
+        <location filename="MainDialog.cpp" line="133"/>
+>>>>>>> 4da7d9fe7b6f3bd9228dbcf5380533f8a39c9e1f
+        <source>Copy</source>
+        <translatorcomment>Copy</translatorcomment>
+        <translation>Copy</translation>
+    </message>
+    <message>
+<<<<<<< HEAD
+        <location filename="StackPickerFrame.cpp" line="133"/>
+=======
+        <location filename="MainDialog.cpp" line="134"/>
+>>>>>>> 4da7d9fe7b6f3bd9228dbcf5380533f8a39c9e1f
+        <source>Copy to clip</source>
+        <translatorcomment>Copy to clip</translatorcomment>
+        <translation>Copy to clip</translation>
+    </message>
+</context>
+<context>
+    <name>TrayMenu</name>
+    <message>
+        <location filename="TrayMenu.cpp" line="11"/>
+        <source>Show</source>
+        <translatorcomment>Show</translatorcomment>
+        <translation>Show</translation>
+    </message>
+    <message>
+        <location filename="TrayMenu.cpp" line="12"/>
+        <source>Pick</source>
+        <translatorcomment>Pick</translatorcomment>
+        <translation>Pick</translation>
+    </message>
+    <message>
+        <location filename="TrayMenu.cpp" line="13"/>
+        <source>Quit</source>
+        <translatorcomment>Quit</translatorcomment>
+        <translation>Quit</translation>
+    </message>
+</context>
+</TS>
